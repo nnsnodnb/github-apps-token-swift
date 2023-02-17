1dba57ab329a1f0ed0a7910246680c01ce85898d
Yuya Oka <nnsnodnb@gmail.com>
2023-02-17 12:54:25 +0900

    Revert "Remove platforms"
    This reverts commit 954b22a4b439d7db12929f24eebe6747f5167f23.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GitHubAppsToken",
+    platforms: [.macOS(.v12)],
     products: [
         .executable(
             name: "github-apps-token",
